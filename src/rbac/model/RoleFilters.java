f3ec81db080fa997f3cb63fdd3f025039a039254
Dkwia <boants@gmail.com>
2026-02-24 11:00:49 +0700

    role filters

---
+++ src/rbac/model/RoleFilters.java
@@ -0,0 +1,24 @@
+public class RoleFilters {
+
+    public static RoleFilter byName(String name) {
+        return role -> role.getName().equals(name);
+    }
+
+    public static RoleFilter byNameContains(String substring) {
+        return role -> role.getName()
+                .toLowerCase()
+                .contains(substring.toLowerCase());
+    }
+
+    public static RoleFilter hasPermission(Permission permission) {
+        return role -> role.hasPermission(permission);
+    }
+
+    public static RoleFilter hasPermission(String name, String resource) {
+        return role -> role.hasPermission(name, resource);
+    }
+
+    public static RoleFilter hasAtLeastNPermissions(int n) {
+        return role -> role.getPermissions().size() >= n;
+    }
+}
