327acf64ad900e6f78785cd89e8c52818002285e
Dkwia <boants@gmail.com>
2026-03-04 01:20:14 +0700

    functional command interface

---
+++ src/rbac/commands/Command.java
@@ -0,0 +1,10 @@
+package rbac.commands;
+
+import rbac.system.RBACSystem;
+
+import java.util.Scanner;
+
+@FunctionalInterface
+public interface Command {
+    void execute(Scanner scanner, RBACSystem system);
+}
