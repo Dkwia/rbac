3bb9faeb1478cc883e490eb06f9a37c684b52346
Dkwia <boants@gmail.com>
2026-03-04 01:36:12 +0700

    removed old init. add new rbac system init

--- src/rbac/Main.java
+++ src/rbac/Main.java
@@ -1,57 +1,31 @@
 package rbac;
 
+import rbac.commands.CommandParser;
+import rbac.commands.CommandRegistry;
+import rbac.system.RBACSystem;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        RBACSystem system = new RBACSystem();
+        system.initialize();
 
-        // Test User validation
-        try {
-            User user = User.validate("admin_01",
-                    "System Administrator",
-                    "admin@example.com");
+        CommandParser parser = new CommandParser();
+        CommandRegistry.registerDefaultCommands(parser);
 
-            System.out.println(user.format());
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+        System.out.println("RBAC console started. Type 'help' for commands.");
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (system.isRunning()) {
+                System.out.print("rbac> ");
+                if (!scanner.hasNextLine()) {
+                    break;
+                }
+                String input = scanner.nextLine();
+                parser.parseAndExecute(input, scanner, system);
+            }
         }
-
-        try {
-            User user = User.validate("admin_02",
-                    "System Administrator2",
-                    "admin2@example.com");
-
-            System.out.println(user.format());
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-
-
-            Permission readUsers =
-                new Permission("read", "USERS", "Can view user list");
-
-        Permission writeUsers =
-                new Permission("write", "USERS", "Can write in user list");
-        Permission deleteUsers =
-                new Permission("delete", "USERS", "Can delete from user list");
-        Role adminRole =
-                new Role("Administrator", "Full system access");
-
-        adminRole.addPermission(readUsers);
-        adminRole.addPermission(writeUsers);
-        adminRole.addPermission(deleteUsers);
-
-        System.out.println(adminRole.format());
-
-        AssignmentMetadata metadata =
-                AssignmentMetadata.now("system", "Initial setup");
-
-        PermanentAssignment assignment =
-                new PermanentAssignment(
-                        User.validate("admin01","Admin User","admin@test.com"),
-                        adminRole,
-                        metadata
-                );
-
-        System.out.println(assignment.summary());
     }
 }
